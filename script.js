d4ebb5982caa9bd09b18c7808d2f4e2523674624
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-16 15:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function findDOMLevel(elementId) {
+      var element = document.getElementById(elementId);
+      var level = 0;
+      
+      while (element.parentElement) {
+        element = element.parentElement;
+        level++;
+      }
+      
+      return level;
+    }
